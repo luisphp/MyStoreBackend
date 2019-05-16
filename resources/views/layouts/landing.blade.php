13aa10c89210d9fe5571dd794b1c7d8489bf64e7
Luis Hurtado <luis.hurtado@linio.com>
2019-05-16 17:01:29 -0400

    Landind adds

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -8,7 +8,13 @@
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
 
+<!--Estilos bootstrap-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<!--Estilos icons-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
 
 
 </head>
@@ -58,73 +64,133 @@
 
 		<div class="row">
 			
-			<div class="col-lg-3 col-md-3">
+			<div class="col-lg-3 col-md-12">
 
-					<div class="card text-center" style="width: 18rem;" >
+					<div class="card text-center">
+
 					  <img src="https://concepto.de/wp-content/uploads/2015/03/paisaje-2-e1549600987975.jpg" class="card-img-top" alt="...">
+
 					  <div class="card-body">
+
 					    <h5 class="card-title">Card title</h5>
+
 					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
 					    <a href="#" class="btn btn-primary">Go somewhere</a>
+
+					    <div style="margin-top: 10px;">
+					    <a href="#" class="card-link">Card link 1</a>
+    					<a href="#" class="card-link">Card link 2</a>
+    					<a href="#" class="card-link">Card link 3</a>
+    					<a href="#" class="card-link">Card link 4</a>
+    					</div>
+
 					  </div>
+
 					  <div class="card-footer text-muted">
 					    2 days ago
 					  </div>
+
 					</div>
 				
 
 			</div>
 
-			<div class="col-lg-9 col-md-9">
+			<div class="col-lg-9 col-md-12">
 
-				<div class="container">
-					<div class="row">
-
-					<div class="col-lg-12">
-
-						<div class="card">
-					  <h4>Brandy... The ocean girl!</h4>
-					  <h6>looking for a song</h6>
-
-					  </div>
-					</div>
-
-					<div class="col-lg-12">
-
-					<div class="card col-lg-6 col-md-6">
-
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						
+							<div class="row" style="margin-bottom: 20px;">
+
+									<div class="col-lg-12">
+
+									  <div class="card">
+									  <h4>Brandy... The ocean girl!</h4>
+									  <h6>looking for a song</h6>
+									  </div>
+
+									</div>
+
+									<div class="col-lg-12" style="height: 400px;">
+
+										<div style="margin-top: 10px;"> 
+
+										<div class="row">
+
+									<div class="col-lg-8 col-md-9 align-self-center">
+
+										<div class="card">
+
+										<p class="text-center align-middle" style="margin-bottom: 60px; margin-top: 60px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										</div>
+
+									</div>
+
+									<div class="col-lg-4 col-md-3 ">
+										<div class="card">
+										<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										</div>
+								</div>
+
+										</div>
+
+										</div>
+								
+								</div>
+						</div>
+
+						
+				<div class="row">
+					<div class="col-lg-4 col-md-4">
+					<div class="card">
+						<img src="https://cdn.pixabay.com/photo/2016/09/01/10/23/image-1635747_960_720.jpg" class="img-fluid" alt="Responsive image" style="max-height: 170px;">
+						<h2 style="margin: 30px;">Holo 1</h2>
 
 					</div>
+					</div>
+					<div class="col-lg-4 col-md-4">
+					<div class="card">
 
-					<div class="card col-lg-6 col-md-6">
 
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						
+						<img src="https://c.wallhere.com/photos/8f/d1/life_park_flowers_trees_summer_plants_baby_brown-628591.jpg!d" class="img-fluid rounded" alt="Responsive image" style="max-height: 170px;">
+						<h2 style="margin: 30px;">Holo 1</h2>
+						<div class="card-body">
+						<button class="btn btn-primary mx-auto d-block">Enviar</button>
+						</div>
+					</div>
+					</div>
+					<div class="col-lg-4 col-md-4">
+					<div class="card">
+						<img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="img-fluid rounded" alt="Responsive image" style="max-height: 170px;">
+						<h2 style="margin: 30px;">Holo 1</h2>
+						<div class="card-body">
+							<div class="row">
+						<button class="btn btn-success mx-auto d-block">Call</button>
+						<button class="btn btn-warning mx-auto d-block">Send</button>
+						</div>
+						</div>
+
+					</div>
+					</div>
+
+				</div>
+
+			
 
 				
 
-					
-
-				</div>
-				
-				</div>
-				</div>
-
 			</div>
 
-			</div>
+			
 
 		</div>
 
